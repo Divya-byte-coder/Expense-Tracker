36be91c5ab5f4045d738905b420da1c413b076a6
Divya-byte-coder <divyaparthiban3011@gmail.com>
2026-04-21 18:13:20 +0530

    added rooot route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 // CORS Configuration
 const allowedOrigins = [
